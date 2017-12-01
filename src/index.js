d4cce60901f35262491c5703a83924c5232e021d
Elliott Alexander <escottalexander@gmail.come.com>
2017-11-30 22:54:31 -0500

    added tones

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { ButtonToolbar, MenuItem, DropdownButton } from 'react-bootstrap';
 
+//Create auido context
+var AudioContext = window.AudioContext || window.webkitAudioContext;
+var context = new AudioContext();
+var o=null
+var g=null
+
+
 class Box extends React.Component {
 	selectBox = () => {
 		this.props.selectBox(this.props.row, this.props.col)
@@ -52,9 +59,9 @@ class Grid extends React.Component {
 
 class Buttons extends React.Component {
 
-handleSelect = (evt) => {
-	this.props.gridSize(evt);
-}
+	handleSelect = (evt) => {
+		this.props.gridSize(evt);
+	}
 
 	render() {
 		return (
@@ -92,7 +99,7 @@ handleSelect = (evt) => {
 class Main extends React.Component {
 	constructor() {
 		super();
-		this.speed = 100;
+		this.speed = 200;
 		this.rows = 30;
 		this.cols = 50;
 
@@ -102,105 +109,126 @@ class Main extends React.Component {
 		}
 	}
 
-	selectBox = (row, col) => {
-		let gridCopy = arrayClone(this.state.gridFull);
-		gridCopy[row][col] = !gridCopy[row][col];
-		this.setState({
-			gridFull: gridCopy
-		});
-	}
 
-	seed = () => {
-		let gridCopy = arrayClone(this.state.gridFull);
-		for (let i = 0; i < this.rows; i++) {
-			for (let j = 0; j < this.cols; j++) {
-				if (Math.floor(Math.random() * 4) === 1) {
-					gridCopy[i][j] = true;
+	toneGen = (type, frequency, x) => {
+		o=context.createOscillator()
+		g=context.createGain()
+		o.connect(g)
+		o.type=type
+		o.frequency.value=frequency
+		g.connect(context.destination)
+		o.start(0)
+		g.gain.exponentialRampToValueAtTime(0.000001,context.currentTime+ x)}
+
+
+		selectBox = (row, col) => {
+			let gridCopy = arrayClone(this.state.gridFull);
+			gridCopy[row][col] = !gridCopy[row][col];
+			this.setState({
+				gridFull: gridCopy
+			});
+		}
+
+		seed = () => {
+			let gridCopy = arrayClone(this.state.gridFull);
+			for (let i = 0; i < this.rows; i++) {
+				for (let j = 0; j < this.cols; j++) {
+					if (Math.floor(Math.random() * 4) === 1) {
+						gridCopy[i][j] = true;
+					}
 				}
 			}
+			this.setState({
+				gridFull: gridCopy
+			});
 		}
-		this.setState({
-			gridFull: gridCopy
-		});
-	}
 
-	playButton = () => {
-		console.log("\x07");
-		clearInterval(this.intervalId);
-		this.intervalId = setInterval(this.play, this.speed);
-	}
+		playButton = () => {
+			clearInterval(this.intervalId);
+			this.intervalId = setInterval(this.play, this.speed);
+		}
 
-	pauseButton = () => {
-		clearInterval(this.intervalId);
-	}
-slow = () => {
-	this.speed = 1000;
-	this.playButton();
-}
+		pauseButton = () => {
+			clearInterval(this.intervalId);
+		}
+		slow = () => {
+			this.speed = 500;
+			this.playButton();
+		}
 
-fast = () => {
-	this.speed = 100;
-	this.playButton();
-}
-clear = () => {
-	var grid = Array(this.rows).fill().map(() => Array(this.cols).fill(false));
-	this.setState({
-		gridFull: grid,
-		generation: 0
-	})
-	this.pauseButton();
-}
-gridSize = (size) => {
-	switch (size) {
-		case "1":
-		this.cols = 20;
-		this.rows = 10;
-		break;
-		case "2":
-		this.cols = 50;
-		this.rows = 30;
-		break;
-		default:
-		this.cols = 50;
-		this.rows = 70;
-	}
-	this.pauseButton();
-	this.clear();
-}
-
-	play = () => {
-		let g = this.state.gridFull;
-		let g2 = arrayClone(this.state.gridFull);
-		for (let i = 0; i < this.rows; i++) {
-			for (let j = 0; j < this.cols; j++) {
-				let count = 0;
-				if (i > 0) if (g[i - 1][j]) count++;
-				if (i > 0 && j > 0) if (g[i - 1][j - 1]) count++;
-				if (i > 0 && j < this.cols - 1) if (g[i - 1][j + 1]) count++;
-				if (j < this.cols - 1) if (g[i][j + 1]) count++;
-				if (j > 0) if (g[i][j - 1]) count++;
-				if (i < this.rows - 1) if (g[i + 1][j]) count++;
-				if (i < this.rows - 1 && j > 0) if (g[i + 1][j - 1]) count++;
-				if (i < this.rows - 1 && this.cols - 1) if (g[i + 1][j + 1]) count++;
-				if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false;
-				if (!g[i][j] && count === 3) g2[i][j] = true;
+		fast = () => {
+			this.speed = 200;
+			this.playButton();
+		}
+		clear = () => {
+			var grid = Array(this.rows).fill().map(() => Array(this.cols).fill(false));
+			this.setState({
+				gridFull: grid,
+				generation: 0
+			})
+			this.pauseButton();
+		}
+		gridSize = (size) => {
+			switch (size) {
+				case "1":
+				this.cols = 20;
+				this.rows = 10;
+				break;
+				case "2":
+				this.cols = 50;
+				this.rows = 30;
+				break;
+				default:
+				this.cols = 50;
+				this.rows = 70;
 			}
+			this.pauseButton();
+			this.clear();
 		}
-		this.setState({
-			gridFull: g2,
-			generation: this.state.generation + 1
-		});
-	}
 
-	componentDidMount() {
-		this.seed();
+		play = () => {
+
+			let g = this.state.gridFull;
+			let g2 = arrayClone(this.state.gridFull);
+			for (let i = 0; i < this.rows; i++) {
+				for (let j = 0; j < this.cols; j++) {
+					let count = 0;
+					if (i > 0) if (g[i - 1][j]) count++;
+					if (i > 0 && j > 0) if (g[i - 1][j - 1]) count++;
+					if (i > 0 && j < this.cols - 1) if (g[i - 1][j + 1]) count++;
+					if (j < this.cols - 1) if (g[i][j + 1]) count++;
+					if (j > 0) if (g[i][j - 1]) count++;
+					if (i < this.rows - 1) if (g[i + 1][j]) count++;
+					if (i < this.rows - 1 && j > 0) if (g[i + 1][j - 1]) count++;
+					if (i < this.rows - 1 && this.cols - 1) if (g[i + 1][j + 1]) count++;
+					if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false;
+					if (!g[i][j] && count === 3) {
+						g2[i][j] = true;
+							if ((Math.floor(Math.random() * 80)) === 1) {
+							this.toneGen("sawtooth",293.7	, 1)
+						} else if ((Math.floor(Math.random() * 80)) === 2) {
+							this.toneGen("sawtooth",370.0, 1)
+						} else if ((Math.floor(Math.random() * 80)) === 3) {
+							this.toneGen("sawtooth",440.0, 1)
+						} 
+					}
+				}
+			}
+			this.setState({
+				gridFull: g2,
+				generation: this.state.generation + 1
+			});
+		}
+
+		componentDidMount() {
+			this.seed();
 		//this.playButton();
 	}
 
 	render() {
 		return (
 			<div>
-			<h1>Conways Melody</h1>
+			<h1>Conway's Melody</h1>
 			<Buttons 
 			playButton={this.playButton}
 			pauseButton={this.pauseButton}
